68854e68f7bdf48cb20a5b45ee117294651c484a
clarkegwapo <23103439@usc.edu.ph>
2024-06-25 10:20:05 +0800

    forgot and update

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -57,6 +57,8 @@ const Homepage = ({token}) => {
     </div>
   </div>
   </div>
+
+  
   )
 }
 
---
+++ src/pages/Updatepass/Updatepass.jsx
@@ -0,0 +1,15 @@
+
+
+import React, { useEffect } from 'react';
+import { supabase } from '../../client';
+
+function PasswordReset() {
+ 
+  return (
+    <div>
+      <p>Resetting password.ASDASSADASDASD..</p>
+    </div>
+  );
+}
+
+export default PasswordReset;
