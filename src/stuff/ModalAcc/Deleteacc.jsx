8b714cc3ca4b02d83aae069df1d05035e9a80561
urmader <21102168@usc.edu.ph>
2024-07-12 10:04:49 +0800

    css

--- src/stuff/ModalAcc/Deleteacc.jsx
+++ src/stuff/ModalAcc/Deleteacc.jsx
@@ -1,38 +1,53 @@
+// Deleteacc.js
+
 import React from 'react';
 import { supabase } from '../../client';
-const Deleteacc = ({ onDelete }) => {
+import styles from './Deleteacc.module.css'; 
 
-    return (
-      <div>
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#delaccmodal" style={{ marginTop: "10px", marginLeft: "10px" }}>RETIRE</button>
-  
-        <div className="modal" id="delaccmodal">
-          <div className="modal-dialog">
-            <div className="modal-content">
-  
-              {/* <!-- Modal Header --> */}
-              <div className="modal-header">
-                <h4 className="modal-title">Retire Acc</h4>
-                <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
-              </div>
-  
-              {/* <!-- Modal body --> */}
-              <div className="modal-body">
-                Sure Ka I Retire ni NIimo?!!!!!
-              </div>
-  
-              {/* <!-- Modal footer --> */}
-              <div className="modal-footer">
-                <button type="button" className="btn btn-danger" onClick={onDelete} data-bs-dismiss="modal">YES</button>
-                <button type='button' className='btn btn-primary' data-bs-dismiss="modal">NO</button>
-              </div>
-  
+const Deleteacc = ({ onDelete }) => {
+  return (
+    <div>
+      <button
+        type="button"
+        className={`btn ${styles['btn-retire']}`} // Apply btn-retire class
+        data-bs-toggle="modal"
+        data-bs-target="#delaccmodal"
+        style={{ marginTop: "10px", marginLeft: "10px" }}
+      >
+        RETIRE
+      </button>
+
+      <div className="modal" id="delaccmodal">
+        <div className="modal-dialog">
+          <div className="modal-content">
+
+            {/* <!-- Modal Header --> */}
+            <div className="modal-header">
+              <h4 className="modal-title">Retire Acc</h4>
+              <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
             </div>
+
+            {/* <!-- Modal body --> */}
+            <div className={`modal-body ${styles.modalBody}`}> {/* Apply CSS class */}
+              Sure Ka I Retire ni NIimo?!!!!!
+            </div>
+
+            {/* <!-- Modal footer --> */}
+            <div className="modal-footer">
+              <button type="button" className="btn btn-danger" onClick={onDelete} data-bs-dismiss="modal">
+                YES
+              </button>
+              <button type='button' className='btn btn-primary' data-bs-dismiss="modal">
+                NO
+              </button>
+            </div>
+
           </div>
         </div>
-  
       </div>
-    )
-  }
 
-export default Deleteacc
+    </div>
+  )
+}
+
+export default Deleteacc;
